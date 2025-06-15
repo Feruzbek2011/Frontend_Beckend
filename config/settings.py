f6022cfd4bda20d8d3474a7b8f0dec66ce5dcfc2
Feruzbek Yusufjonov <156335598+Feruzbek2011@users.noreply.github.com>
2025-06-15 18:00:10 +0300

    Update settings.py
    h

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'https://article-word.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'article-word.onrender.com']
 
 
 # Application definition
