16a166f5d0e960410d93eb60343042825371c7be
Feruzbek Yusufjonov <156335598+Feruzbek2011@users.noreply.github.com>
2025-06-17 20:48:32 +0300

    Update settings.py
    v

--- config/settings.py
+++ config/settings.py
@@ -27,8 +27,6 @@ SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
-print("DEBUG:", DEBUG)  # bu qatordan keyin qo'yilsin
-
 ALLOWED_HOSTS = ['127.0.0.1', 'article-word.onrender.com']
 
 
