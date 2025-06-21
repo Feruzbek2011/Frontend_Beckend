5382d63de0c20b4a00b9cc511d0352ad6ac85a6f
Feruzbek Yusufjonov <156335598+Feruzbek2011@users.noreply.github.com>
2025-06-21 14:39:54 +0300

    Update models.py
    b

--- post/models.py
+++ post/models.py
@@ -9,7 +9,7 @@ class Post(models.Model):
         title = models.CharField(max_length=200)
         summary = models.CharField(max_length=250, blank=True)
         body = RichTextField()
-        photo = models.ImageField(upload_to='images' ,blank=True)
+        photo = models.CharField(max_length=255))
         date = models.DateField(auto_now_add=True)
         author = models.ForeignKey(
             get_user_model(),
