aabafccf7ddc0504974f621918e2f7537db159a3
Feruzbek Yusufjonov <156335598+Feruzbek2011@users.noreply.github.com>
2025-06-21 13:40:28 +0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['127.0.0.1', 'article-word.onrender.com']
 
 
